3b091832234914f430199f3f49fa2ec0274e4f71
Janet Wan <Janet.Wan@capitalone.com>
2016-04-29 23:17:25 -0500

    adding comments

--- features/support/pages/HomePage.rb
+++ features/support/pages/HomePage.rb
@@ -8,6 +8,7 @@ def homepage
   element.click()
 end
   def clickOnAllproducts
+    # Selects IPhone from all products drop-down
     products=  browser.find_element(:id=> "menu-item-33")
      browser.action.move_to(products).perform
     sleep(2)
@@ -15,6 +16,7 @@ end
     iphone.click
   end
   def selectIphone
+    # Selects IPhone and adds it to cart
     iphone =browser.find_element(:xpath=> '//*[@id="default_products_page_container"]/div[4]/div[2]/form/div[2]/div[1]/span/input')
     iphone.click()
     sleep(2)
@@ -25,12 +27,14 @@ end
   end
   def shippinginformation
     sleep(2)
+    # Selects Albania as country for shipping cost
     country =browser.find_element(:id=> 'uniform-current_country')
     country.click()
     country.find_elements( :tag_name => "option" ).find do |option|
       option.text == 'Albania'
     end.click
     country.click
+    # Fills out information for shipping
     browser.find_element(:id=>'wpsc_checkout_form_9').send_keys("janetwan2804@gmail.com")
     browser.find_element(:id=>'wpsc_checkout_form_2').send_keys("Janet")
     browser.find_element(:id=>'wpsc_checkout_form_3').send_keys("Wan")
@@ -47,9 +51,11 @@ end
     browser.find_element(:id=>'shippingSameBilling').click()
     browser.find_element(:xpath=>'//*[@id="wpsc_shopping_cart_container"]/form/div[4]/div/div/span/input').click
     total=browser.find_element(:id=>'checkout_total')
+   #Validates total cost for items
     total.text.equal?("$293.00")
   end
   def addtocart
+    # Adds three items to cart
     for i in (0..2)
     iphone =browser.find_element(:xpath=> '//*[@id="default_products_page_container"]/div[4]/div[2]/form/div[2]/div[1]/span/input')
     iphone.click()
@@ -64,6 +70,7 @@ end
     browser.find_element(:class=>'cart_icon').click
   end
   def validatecart
+    # Validates that removing all items from cart clears the cart
     browser.find_element(:name=>'quantity').attribute('value') == "3"
     browser.find_element(:xpath=>'//*[@id="checkout_page_container"]/div[1]/table/tbody/tr[2]/td[6]/form/input[4]').click
     if  browser.page_source.include? 'Oops, there is nothing in your cart.'
